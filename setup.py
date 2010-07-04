528dbe4feba1b7f433c800e24dd9e6d76ebbfc09
Mehdi Rejraji <mehdi@portable-mehdi>
2010-07-04 14:36:41 +0200

    commit before release

--- setup.py
+++ setup.py
@@ -89,7 +89,7 @@ class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
 
 DistUtilsExtra.auto.setup(
     name='weather-indicator',
-    version='10.07.6',
+    version='10.07.7',
     license='GPL-3',
     author='Sebastian MacDonald | Mehdi Rejraji',
     author_email='sebas310@gmail.com',
