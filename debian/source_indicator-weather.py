9032863d81fd703863b9ef8a9a03ecc12244726c
Vadim Rutkovsky <roignac@gmail.com>
2011-03-13 14:13:24 +0200

    Removing :00 seconds from sunset/sunrise time

--- debian/source_indicator-weather.py
+++ debian/source_indicator-weather.py
@@ -5,7 +5,6 @@ def add_info(report, ui=None):
     if not apport.packaging.is_distro_package(report['Package'].split()[0]):
         report['ThirdParty'] = 'True'
         report['CrashDB'] = 'indicator_weather'
-    report['settings'] = command_output(['gconftool-2', '-R', '/apps/indicator-weather'])
     log_filename = os.path.join(os.path.expanduser("~/.cache"), "indicator-weather.log")
     if os.path.exists(log_filename):
         report['IndicatorLog'] = open(log_filename).read()
